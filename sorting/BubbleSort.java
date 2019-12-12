48b6abd8f406962069d5a7ababd1bc7a83b93754
Barkin Ak <barkinak@outlook.com>
2019-12-12 23:59:43 +0300

    Bubble Sort

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,20 @@
+package hackerrank.sorting;
+
+public class BubbleSort {
+
+    static int bubbleSort(int arr[]) {
+        int n = arr.length;
+        int count = 0;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1]) {
+                    // swap arr[j+1] and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    count++;
+                }
+        return count;
+    }
+    
+}
